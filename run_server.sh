1fd4534bbb1b66fd8e2545338d7cabaf4a04b99d
Gifi42 <43057351+Gifi42@users.noreply.github.com>
2022-01-07 05:27:51 +0400

    Update run_server.sh

--- run_server.sh
+++ run_server.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Update server files
-/usr/games/steamcmd +force_install_dir /server +login arg_gifi HnAmdA.p69_rB-c +app_update $STEAM_APP_ID validate +quit
+/usr/games/steamcmd +login anonymous +force_install_dir /server +app_update $STEAM_APP_ID validate +quit
 
 # Run server
 /server/DayZServer -cpuCount=$CPU_COUNT -dologs -adminlog -netlog -freezecheck -profiles="${PROFILE_DIR}" -config="${CONFIG_FILE}"
